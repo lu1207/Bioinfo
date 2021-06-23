91d6df81c3cab1dfb0d35dfe59aedb9d1b1dedee
lu1207 <86335582+lu1207@users.noreply.github.com>
2021-06-22 21:56:51 -0500

    Add files via upload

---
+++ assigment_8.R
@@ -0,0 +1,12 @@
+
+
+Remove_non_numeric <- function(raw_data)
+  {for (i in 1:length(names(raw_data))) 
+    {while (class(raw_data[,i]) != "integer") 
+      {raw_data <- raw_data[-c(i)]
+      }
+    }
+  return(raw_data) 
+}
+
+
